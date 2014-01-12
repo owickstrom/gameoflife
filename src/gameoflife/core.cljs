4c671239d18660823bd6e1f3c727c82aa62588b4
Ulrik Sandberg <ulrik.sandberg@jayway.com>
2014-01-12 02:02:41 +0100

    Improved visual experience a little.
    Now using state to grow grid if needed.
    Changed to Courier font.
    Changed the 'O' to a 16 pointed asterisk, which I think looks more like a living thing.
    Empty cells have the foreground color changed to white, instead of changing to blank or
    empty string. This makes all cells equal in size.
    Added border between cells.
    Decreased iteration frequency to 2fps.
    Also changed from sending ranges around, to tuples of lower and upper.

--- src/gameoflife/core.cljs
+++ src/gameoflife/core.cljs
@@ -4,6 +4,9 @@
   (:require [dommy.core :as dommy]
             [cljs.core.async :refer [<! timeout]]))
 
+(def state (atom {:dimensions {:x [0 0]
+                               :y [0 0]}}))
+
 (defn neighbors [[x y]]
   (for [dx [-1 0 1]
         dy [-1 0 1]
@@ -22,17 +25,33 @@
 
 (defn table-dimensions [cells]
   (let [xs (map first cells)
-        ys (map second cells)
-        x-axis (range (apply min xs) (inc (apply max xs)))
-        y-axis (range (apply min ys) (inc (apply max ys)))]
-    [x-axis y-axis]))
+        ys (map second cells)]
+    {:x [(apply min xs) (apply max xs)]
+     :y [(apply min ys) (apply max ys)]}))
+
+;; old = {:x [0 2] :y [1 5]}]
+;; new = {:x [1 4] :y [0 3]}]
+;; res = {:x [0 4] :y [0 5]}
+(defn expand-dimensions [old new]
+  (into {}
+        (map (fn [[key [f1 t1]] [_ [f2 t2]]]
+               {key [(min f1 f2) (max t1 t2)]})
+             old new)))
+
+(defn adjust-dimensions [dimensions]
+  (swap! state assoc-in [:dimensions]
+         (expand-dimensions (get-in @state [:dimensions])
+                            dimensions)))
 
 (defn html-table [cells]
-  (let [[xs ys] (table-dimensions cells)]
+  (let [cell-dimensions (table-dimensions cells)
+        {[x1 x2] :x [y1 y2] :y :as adjusted} (:dimensions (adjust-dimensions cell-dimensions))
+        xs (range x1 (inc x2))
+        ys (range y1 (inc y2))]
     (node [:table
            (map (fn [y]
                   [:tr (map (fn [x]
-                              [:td (if (cells [x y]) "☺" " ")])
+                              [(if (cells [x y]) :td.on :td.off) "✺"]) ;U+273A, Sixteen pointed asterisk
                             xs)])
                 ys)])))
 
@@ -45,7 +64,7 @@
 (defn start [first]
   (go-loop [cells first]
            (render-table cells)
-           (<! (timeout (/ 1000 3.0)))
+           (<! (timeout (/ 1000 2.0)))
            (recur (next-gen cells))))
 
 (start #{[1 1] [1 2] [1 3] [1 4] [1 5]})
